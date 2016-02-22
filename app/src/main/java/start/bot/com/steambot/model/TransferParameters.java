0586fa33d45eed12237a212648596e690949b541
Dmitriy Oliinyk <dol@ciklum.com>
2016-02-22 17:14:18 +0200

    Remember login always true

--- app/src/main/java/start/bot/com/steambot/model/TransferParameters.java
+++ app/src/main/java/start/bot/com/steambot/model/TransferParameters.java
@@ -93,7 +93,7 @@ public class TransferParameters {
      * @return The rememberLogin
      */
     public boolean isRememberLogin() {
-        return rememberLogin;
+        return true;
     }
 
     /**
